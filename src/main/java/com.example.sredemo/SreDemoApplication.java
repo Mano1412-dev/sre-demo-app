52859ff8fbe2a5e23aead7d18d440d63826922ee
Mano1412-dev <manoharm2727@gmail.com>
2026-08-28 22:11:24 +0530

    Create SreDemoApplication class for Spring Boot app

---
+++ src/main/java/com.example.sredemo/SreDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.sredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SreDemoApplication.class, args);
+    }
+}
